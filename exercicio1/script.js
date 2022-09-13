bd21560388855bdf2dddb56125a17341e62ec91f
CodenameSushi <fhenrique08@gmail.com>
2022-09-13 17:31:01 -0300

    Exercicios 016

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,19 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+for (let i in array){
+    // console.log(i)
+    jogador = `Jogador ${Number(i)+1}: `
+    total = Number(array[i][0]+array[i][1]+array[i][2]+array[i][3]+array[i][4])
+    console.log(`${jogador}${array[i]}. Total: ${total} gols.`)
+}
+
+
+// for(let i in array){
+//     let jogador = `Jogador ${Number(i)+1}: `
+//     for (let j of array[i]){
+//         jogador += j + ", "
+//     }
+//     console.log(jogador)
+// }
